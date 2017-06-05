f32bae82f27f1ec7cb64f911db1ece3d9faf041f
GuoCHLiam <guochang79@gmail.com>
2017-06-06 04:48:56 +0800

    movie detail page frame

--- app/src/main/java/com/eatpotteam/eatpotmovie/moviedetailpage/MovieDetailActivity.java
+++ app/src/main/java/com/eatpotteam/eatpotmovie/moviedetailpage/MovieDetailActivity.java
@@ -1,15 +1,42 @@
 package com.eatpotteam.eatpotmovie.moviedetailpage;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.eatpotteam.eatpotmovie.R;
+import com.eatpotteam.eatpotmovie.showingspage.ShowingsActivity;
 
 public class MovieDetailActivity extends AppCompatActivity {
+    private ImageView poster;
+    private TextView title;
+    private TextView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        poster = (ImageView) findViewById(R.id.iv_detail_poster);
+        title = (TextView) findViewById(R.id.tv_detail_title);
+        info = (TextView) findViewById(R.id.tv_detail_info);
+
+        requestMovieInfo(getIntent().getIntExtra("movieId", 0));
+
+        findViewById(R.id.btn_detail_buy).setOnClickListener(
+                view -> startActivity(
+                        new Intent(MovieDetailActivity.this,
+                                ShowingsActivity.class)));
     }
+
+    private void requestMovieInfo(int movieId) {
+        poster.setImageBitmap(BitmapFactory.decodeFile(getLocalPoster(movieId)));
+        title.setText(getMovieTitle(movieId));
+        info.setText(getMovieInfo(movieId));
+    }
+
+
 }
